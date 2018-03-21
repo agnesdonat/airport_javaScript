749f30ec29dcdd7a7edf6983af3a3be1559b4ca2
Vivian Allen <mrvivianallen@gmail.com>
2018-03-21 15:26:14 +0000

    plane takeoff and confirmation features red->green

--- spec/airportChallengeSpec.js
+++ spec/airportChallengeSpec.js
@@ -5,4 +5,11 @@ describe('Airport', function() {
     airport.land(plane);
     expect(airport.planes()).toContain(plane);
   });
+  it('can make a plane take off', function () {
+    var airport = new Airport();
+    var plane = new Plane();
+    airport.land(plane);
+    airport.take_off(plane);
+    expect(airport.isPlaneDeparted(plane)).toEqual(true);
+  });
 });
--- src/airportChallenge.js
+++ src/airportChallenge.js
@@ -10,6 +10,15 @@ Airport.prototype.land = function(plane) {
   this._planes.push(plane);
 }
 
+Airport.prototype.take_off = function(plane) {
+  var i = this._planes.indexOf(plane);
+  this._planes.splice(i,1);
+}
+
+Airport.prototype.isPlaneDeparted = function(plane) {
+  return !this._planes.includes(plane);
+}
+
 function Plane() {
 
 }
